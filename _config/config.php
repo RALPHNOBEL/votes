831cbf93395dcdbb36baf3c7409028aaa1315ea1
RALPHNOBEL <ralphloicnobel@gmail.com>
2026-03-27 20:05:19 +0100

    routeur

--- _config/config.php
+++ _config/config.php
@@ -1,6 +1,4 @@
 <?php
-
-// A Enlever lors du deploiement
 error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', true);
 
@@ -8,15 +6,14 @@ ini_set('display_errors', true);
 ini_set('session.cookie_lifetime', false);
 session_start();
 
-// CONSTANTS
+// CONSTANTS - PATH dynamique (fonctionne local ET Railway)
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+$host = $_SERVER['HTTP_HOST'];
+define('PATH_REQUIRE', __DIR__ . '/../');
+define('PATH', $protocol . '://' . $host . '/');
 
-// PATH
-define('PATH_REQUIRE', substr($_SERVER['SCRIPT_FILENAME'], 0, -9));
-define('PATH', substr($_SERVER['PHP_SELF'], 0, -9));
-
-
-// DATABASE INFORMATION
-define("DATABASE_HOST", "localhost");
-define("DATABASE_NAME", "ges_vote");
-define("DATABASE_USER", "root");
-define("DATABASE_PASSWORD", "");
+// DATABASE - Variables d'environnement Railway
+define("DATABASE_HOST", getenv('PGHOST') ?: getenv('MYSQLHOST') ?: 'localhost');
+define("DATABASE_NAME", getenv('PGDATABASE') ?: getenv('MYSQLDATABASE') ?: 'ges_vote');
+define("DATABASE_USER", getenv('PGUSER') ?: getenv('MYSQLUSER') ?: 'root');
+define("DATABASE_PASSWORD", getenv('PGPASSWORD') ?: getenv('MYSQLPASSWORD') ?: '');
